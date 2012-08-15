fbe4a0e8a9f4a1284b97c4524c6c2e7b36bc7a77
Zachary Alex Stern <zs@enternewmedia.com>
2012-08-15 16:09:14 -0400

    fixed config.pp errors for apache2 module

--- apache2/manifests/config.pp
+++ apache2/manifests/config.pp
@@ -3,9 +3,9 @@ class apache2::config {
   $conf_file=$apache2::params::conf_file
   $conf_path=$apache2::params::conf_path
   file { 'apache2_config':
-    source => "puppet:///modules/apache2/${conf_file}",
     ensure => file,
-    path   => "$apache2_config_path",
+    source => "/etc/puppetlabs/puppet/modules/apache2/files/${conf_file}",
+    path   => $apache2_config_path,
     owner  => 'root',
     before => Class['apache2::service'],
   }
